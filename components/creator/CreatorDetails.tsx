826a76533e53ddecea91da183620b24d1af818a1
Osaretin Frank <osaretin.frank10@gmail.com>
2024-08-21 04:28:36 +0100

    creator page update

--- components/creator/CreatorDetails.tsx
+++ components/creator/CreatorDetails.tsx
@@ -5,6 +5,8 @@ import { BiHeart, BiUser } from "react-icons/bi"
 import { BsStarFill } from "react-icons/bs"
 import { FcRating } from "react-icons/fc"
 import { GoVerified } from "react-icons/go"
+import { motion } from "framer-motion"
+import { FaPlay } from "react-icons/fa6"
 
 export const CreatorDetails = () => {
   return (
@@ -50,15 +52,15 @@ export const CreatorDetails = () => {
           <p>Aug, 2024</p>
         </div>
         <div>
-          <p>Rating</p>
+          <p>Stars</p>
           <div className="flex items-center justify-center gap-2">
             <BsStarFill className="text-green-400" />
-            <p className="font-semibold text-[15px]">4.8</p>
+            <p className="font-semibold text-[15px]">48</p>
           </div>
         </div>
       </div>
 
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 my-7">
         <Button className="md:w-[200px] h-[45px]" size="large" type="primary">
           Subcribe
         </Button>
@@ -70,6 +72,66 @@ export const CreatorDetails = () => {
         >
           Like
         </Button>
+      </div>
+
+      <div className="md:mt-10">
+        <p className="font-arvo text-sm font-semibold mb-4">Suggested songs</p>
+        <div className="overflow-x-auto md:w-[600px] flex items-center gap-4 no-scrollbar relative">
+          <div className="relative">
+            <div className="bg-yellow-500 h-[2rem] w-[3rem] rounded-full z-[-1] absolute top-0 left-[1rem]" />
+            <div className="bg-primary-default h-[2rem] w-[3rem] rounded-full z-[-1] absolute bottom-0 right-[1rem]" />
+
+            <div className="bg-dark-700/15 p-3 pr-6 rounded-lg inline-flex items-center justify-between min-w-[350px] relative backdrop-blur-[45px] border-[1px] border-white/5">
+              <div className="flex items-center gap-3">
+                <Avatar
+                  size={70}
+                  className="bg-green-500/15 rounded-lg"
+                  src="https://str.vercel.app/_next/image?url=%2Fnft2.png&w=2048&q=100"
+                >
+                  <BiUser />
+                </Avatar>
+                <div>
+                  <p className="font-semibold">On the Hill</p>
+                  <p className="text-sm text-white/70">The Chase</p>
+                </div>
+              </div>
+
+              <Avatar
+                className="bg-primary-default cursor-pointer shadow-lg"
+                size={35}
+              >
+                <FaPlay size={15} />
+              </Avatar>
+            </div>
+          </div>
+          <div className="relative">
+            <div className="bg-yellow-500 h-[2rem] w-[3rem] rounded-full z-[-1] absolute top-0 left-[1rem]" />
+            <div className="bg-primary-default h-[2rem] w-[3rem] rounded-full z-[-1] absolute bottom-0 right-[1rem]" />
+
+            <div className="bg-dark-700/15 p-3 pr-6 rounded-lg inline-flex items-center justify-between min-w-[350px] relative backdrop-blur-[45px] border-[1px] border-white/5">
+              <div className="flex items-center gap-3">
+                <Avatar
+                  size={70}
+                  className="bg-green-500/15 rounded-lg"
+                  src="https://cdn.pixabay.com/photo/2023/10/14/14/22/man-8314982_1280.jpg"
+                >
+                  <BiUser />
+                </Avatar>
+                <div>
+                  <p className="font-semibold">On the Hill</p>
+                  <p className="text-sm text-white/70">The Chase</p>
+                </div>
+              </div>
+
+              <Avatar
+                className="bg-primary-default cursor-pointer shadow-lg"
+                size={35}
+              >
+                <FaPlay size={15} />
+              </Avatar>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
